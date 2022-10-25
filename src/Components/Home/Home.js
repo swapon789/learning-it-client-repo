5a57dda6ea11422087c3b05db161080f0149f1be
swapon789 <kumarshopon850@gmail.com>
2022-10-25 12:08:10 +0600

    Home section added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import pic from '../../image/header image.jpg'
+
+const Home = () => {
+    return (
+        <div>
+            <img style={{height:"480px"}} src={pic} alt="" />
+        </div>
+    );
+};
+
+export default Home;
