aeb2351a0a1def1f90568036460b0c94b3b0a2c9
swapon789 <kumarshopon850@gmail.com>
2022-10-25 11:35:30 +0600

    router path added

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layout/Main';
+import Blog from '../Blog/Blog';
+import Course from '../Course/Course';
+import FAQ from '../FAQ/FAQ';
+import Home from '../Home/Home';
 
 export const routes = createBrowserRouter([
-    
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/course',
+                element:<Course></Course>
+            },
+            {
+                path:'/blog',
+                element:<Blog></Blog>
+            },
+            {
+                path:'/faq',
+                element:<FAQ></FAQ>
+            }
+        ]
+    }
 ])
